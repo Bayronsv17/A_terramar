d60250d6b401458251af4b65b625f225b6decf3b
Bayronsv17 <baarciniegaza@ittepic.edu.mx>
2025-12-10 08:53:57 -0700

    versionalfa2

--- components/Footer.js
+++ components/Footer.js
@@ -1,3 +1,5 @@
+import Link from 'next/link'
+
 export default function Footer() {
   return (
     <footer className="bg-gray-900 text-gray-200 py-8">
@@ -5,8 +7,8 @@ export default function Footer() {
         <div className="flex flex-col md:flex-row justify-between items-center">
           <div className="mb-4 md:mb-0">&copy; {new Date().getFullYear()} AlmaTerramar</div>
           <div className="space-x-4">
-            <a href="/terminos" className="hover:underline">Términos</a>
-            <a href="/privacidad" className="hover:underline">Privacidad</a>
+            <Link href="/terminos" className="hover:underline">Términos</Link>
+            <Link href="/privacidad" className="hover:underline">Privacidad</Link>
           </div>
         </div>
       </div>
